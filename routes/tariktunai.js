f0d53e6fb6c809184891440279bc1ba0144c10a8
snooters <cybershane11@gmail.com>
2023-03-02 23:42:51 +0700

    c

--- routes/tariktunai.js
+++ routes/tariktunai.js
@@ -348,6 +348,49 @@ router.post('/', async (req, res) => {
                     status_rek: "AKTIF"
                 }
             });
+        } else {
+            if (trx_type == "REV") {
+                getprint("RESPONSE RELEASE RELEASE TOKEN ", {
+                    code: Successful,
+                    status: "SUKSES",
+                    message: "SUKSES",
+                    rrn: rrn,
+                    data: {
+                        bpr_id: bpr_id,
+                        trx_code: trx_code,
+                        trx_type: trx_type,
+                        no_hp: no_hp,
+                        no_rek: no_rek,
+                        nama: nama_dr,
+                        amount: amount,
+                        trans_fee: trans_fee,
+                        tgl_trans: tgl_trans,
+                        tgl_transmis: tgl_transmis,
+                        noreff: tgl_trans.substr(0, 8) + rrn,
+                        status_rek: "AKTIF"
+                    }
+                });
+                return res.status(200).send({
+                    code: Successful,
+                    status: "SUKSES",
+                    message: "SUKSES",
+                    rrn: rrn,
+                    data: {
+                        bpr_id: bpr_id,
+                        trx_code: trx_code,
+                        trx_type: trx_type,
+                        no_hp: no_hp,
+                        no_rek: no_rek,
+                        nama: nama_dr,
+                        amount: amount,
+                        trans_fee: trans_fee,
+                        tgl_trans: tgl_trans,
+                        tgl_transmis: tgl_transmis,
+                        noreff: tgl_trans.substr(0, 8) + rrn,
+                        status_rek: "AKTIF"
+                    }
+                });
+            }
         }
 
 
